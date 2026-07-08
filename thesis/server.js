56d7f769de607b520cbdfd682f39906ba26ff015
winners55558-png <winners55558@gmail.com>
2026-07-08 23:49:37 +0700

    Add files via upload

--- thesis/server.js
+++ thesis/server.js
@@ -22,7 +22,7 @@ const { OAuth2Client } = require('google-auth-library');
 const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
